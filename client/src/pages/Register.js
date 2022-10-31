0a04dab73d8fbb0f0e49c353f44a3b7666026b77
margoshkari <68592214+margoshkari@users.noreply.github.com>
2022-10-31 14:53:36 +0200

    register validation

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -17,15 +17,50 @@ function RegisterApp({ updateRegister }) {
         if (data.isRegister) {
           updateRegister(true);
         } else {
+          console.log(data.error.errors[0].msg);
+          ShowValidationMessage(
+            data.error.errors[0].param,
+            data.error.errors[0].msg
+          );
           updateRegister(false);
         }
       });
   }
+  function ShowValidationMessage(param, msg) {
+    let fields = document.getElementsByClassName("field");
+    Array.from(fields).forEach((element) => {
+      element.style = "border: 1px solid black";
+    });
+    switch (param) {
+      case "username":
+        document.getElementById("username").style = "border: 1px solid red";
+        break;
+      case "password":
+        document.getElementById("password").style = "border: 1px solid red";
+        break;
+    }
+    let span = document.getElementById("error_message");
+    span.style = "color: red;";
+    span.innerHTML = msg;
+  }
   return (
     <div className="Registration">
       <h2>Register</h2>
-      <input type="text" placeholder="Nickname" id="username"></input>
-      <input type="password" placeholder="Password" id="password"></input>
+
+      <input
+        type="text"
+        placeholder="Nickname"
+        id="username"
+        className="field"
+      ></input>
+      <input
+        type="password"
+        placeholder="Password"
+        id="password"
+        className="field"
+      ></input>
+
+      <span id="error_message"></span>
       <button onClick={Register}>Register</button>
     </div>
   );
--- server/app.js
+++ server/app.js
@@ -7,49 +7,70 @@ const Products = require("./models/ProductModel");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const token = require("./jwt");
+const { check, validationResult } = require("express-validator");
 
 require("dotenv").config();
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.post("/api/register", async (req, res) => {
-  const { username, password } = req.body;
-  bcrypt.hash(password, 10).then(async (hash) => {
-    var user = new Users.User({
-      username: username,
-      password: hash,
-      role: "user",
-    });
-    await user
-      .save()
-      .then(() => {
-        res.json({ isRegister: true });
-      })
-      .catch((err) => {
-        if (err) {
-          res.status(400).json({ error: err });
-        }
+app.post(
+  "/api/register",
+  [
+    check("username", "Username cannot be empty!").notEmpty(),
+    check("username", "Username length should be between 5 and 10!").isLength({
+      min: 5,
+      max: 10,
+    }),
+    check("password", "Password cannot be empty!").notEmpty(),
+    check("password", "Password length should be between 5 and 10!").isLength({
+      min: 5,
+      max: 10,
+    }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      console.log(errors);
+      return res.status(400).json({ error: errors });
+    }
+    const { username, password } = req.body;
+    bcrypt.hash(password, 10).then(async (hash) => {
+      var user = new Users.User({
+        username: username,
+        password: hash,
+        role: "user",
       });
-  });
-});
+      await user
+        .save()
+        .then(() => {
+          res.json({ isRegister: true });
+        })
+        .catch((err) => {
+          if (err) {
+            res.status(400).json({ error: err });
+          }
+        });
+    });
+  }
+);
 
 app.post("/api/login", async (req, res) => {
   const { username, password } = req.body;
   const user = await Users.User.findOne({ username: username });
   if (!user) {
-    res.status(400).json({ error: "User doesn't exist" });
+    return res.status(400).json({ error: "User doesn't exist" });
   }
   const hashPassword = user.password;
   bcrypt.compare(password, hashPassword).then(async (match) => {
     if (!match) {
-      res.status(400).json({ error: "Incorrect password" });
+      return res.status(400).json({ error: "Incorrect password" });
     } else {
       const accessToken = token.createToken(user);
       res.cookie("access-token", accessToken, {
         maxAge: 60 * 60 * 24 * 30 * 1000,
       });
-      res.json({ isLogin: true, role: user.role });
+      return res.json({ isLogin: true, role: user.role });
     }
   });
 });
@@ -75,9 +96,11 @@ app.get("/api/products", async (req, res) => {
   res.json({ products: productsArray });
 });
 // подключение
-mongoose
-  .connect(process.env.mongooseConnection)
-  .catch((err) => console.log(err));
+mongoose.connect(process.env.mongooseConnection, function (err, db) {
+  if (!err) {
+    console.log("We are connected");
+  } else console.log(err);
+});
 
 app.listen(port, () => {
   console.log("Server start");
